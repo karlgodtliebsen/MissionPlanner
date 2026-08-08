1bfd88ff0bc050475e4e513d5248e3835fe665ca
karlgodtliebsen <karl@godtliebsen.dk>
2026-08-08 02:36:04 +0200

    More fitting of the UI for Flight Planner

--- src/UI/MissionPlanner.App/Views/Missions/MissionItemListView.xaml.cs
+++ src/UI/MissionPlanner.App/Views/Missions/MissionItemListView.xaml.cs
@@ -11,6 +11,20 @@ namespace MissionPlanner.App.Views.Missions;
 public partial class MissionItemListView : ExtendedContentView<MissionItemListViewModel>
 {
     /// <summary>
+    /// Gets or sets a value indicating whether all rows are displayed.
+    /// </summary>
+    public bool ShowAllRows
+    {
+        get => (bool)GetValue(ShowAllRowsProperty);
+        set => SetValue(ShowAllRowsProperty, value);
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="ShowAllRows"/> bindable property.
+    /// </summary>
+    public static readonly BindableProperty ShowAllRowsProperty = BindableProperty.Create(nameof(ShowAllRows), typeof(bool), typeof(MissionItemListView), true);
+
+    /// <summary>
     /// Initializes a new instance of the <see cref="MissionItemListView"/> class.
     /// </summary>
     public MissionItemListView()
