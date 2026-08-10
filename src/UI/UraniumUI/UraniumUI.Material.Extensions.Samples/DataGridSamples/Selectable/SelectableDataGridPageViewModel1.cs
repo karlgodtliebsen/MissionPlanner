9f1a44fa55612cb3bfb3d761fe06036a7287243d
karlgodtliebsen <karl@godtliebsen.dk>
2026-08-10 02:49:54 +0200

    Updated VirtualizationDataGrid with more selectable features

--- src/UI/UraniumUI/UraniumUI.Material.Extensions.Samples/DataGridSamples/Selectable/SelectableDataGridPageViewModel1.cs
+++ src/UI/UraniumUI/UraniumUI.Material.Extensions.Samples/DataGridSamples/Selectable/SelectableDataGridPageViewModel1.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.ObjectModel;
-using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Mapsui.Utilities;
 using UraniumUI.Extensions;
 using UraniumUI.Material.Extensions.Samples.DataGrids.Models;
@@ -11,29 +11,66 @@ namespace UraniumUI.Material.Extensions.Samples.DataGridSamples.Selectable;
 public partial class SelectableDataGridPageViewModel1 : ObservableObject
 {
     public ObservableRangeCollection<CustomDataGridStudent> Items { get; } = [];
+
     public ObservableCollection<CustomDataGridStudent> SelectedItems { get; } = [];
 
-    public ICommand RemoveSelectedCommand { get; set; }
+    [ObservableProperty] public partial CustomDataGridStudent? SelectedItem { get; set; }
 
     private StudentDataStore DataStore { get; } = new StudentDataStore();
 
     public SelectableDataGridPageViewModel1()
     {
         Initialize().FireAndForget();
-
-        RemoveSelectedCommand = new Command(() =>
-        {
-            var allItems = Items.ToList();
-            foreach (var customDataGridStudent in SelectedItems)
-            {
-                allItems.Remove(customDataGridStudent);
-            }
-
-            Items.Clear();
-            Items.AddRange(allItems);
-            SelectedItems.Clear();
-        });
     }
+
+    private bool CanRemoveSelectedSingle()
+    {
+        return SelectedItem is not null;
+    }
+
+    private bool CanRemoveSelected()
+    {
+        return SelectedItems.Any();
+    }
+
+    [RelayCommand(CanExecute = nameof(CanRemoveSelectedSingle))]
+    private void RemoveSelectedSingle()
+    {
+        if (SelectedItem is null)
+        {
+            return;
+        }
+
+        var allItems = Items.ToList();
+        allItems.Remove(SelectedItem);
+
+        Items.Clear();
+        Items.AddRange(allItems);
+        SelectedItems.Clear();
+    }
+
+    [RelayCommand(CanExecute = nameof(CanRemoveSelected))]
+    private void RemoveSelected()
+    {
+        var allItems = Items.ToList();
+        foreach (var customDataGridStudent in SelectedItems)
+        {
+            allItems.Remove(customDataGridStudent);
+        }
+
+        Items.Clear();
+        Items.AddRange(allItems);
+        SelectedItems.Clear();
+    }
+
+
+    [RelayCommand]
+    private void SelectionChanged()
+    {
+        RemoveSelectedSingleCommand.NotifyCanExecuteChanged();
+        RemoveSelectedCommand.NotifyCanExecuteChanged();
+    }
+
 
     private async Task Initialize()
     {
--- src/UI/UraniumUI/UraniumUI.Material.Extensions.Samples/DataGridSamples/Selectable/SelectableDataGridPageViewModel2.cs
+++ src/UI/UraniumUI/UraniumUI.Material.Extensions.Samples/DataGridSamples/Selectable/SelectableDataGridPageViewModel2.cs
@@ -1,5 +1,5 @@
-﻿using System.Windows.Input;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Mapsui.Utilities;
 using UraniumUI.Extensions;
 using UraniumUI.Material.Extensions.Samples.DataGrids.Models;
@@ -11,26 +11,36 @@ public partial class SelectableDataGridPageViewModel2 : ObservableObject
 {
     public ObservableRangeCollection<SelectableCustomDataGridStudent> Items { get; private set; } = [];
 
-    public ICommand RemoveSelectedCommand { get; set; }
-
     private StudentDataStore DataStore { get; } = new StudentDataStore();
 
     public SelectableDataGridPageViewModel2()
     {
         Initialize().FireAndForget();
+    }
 
-        RemoveSelectedCommand = new Command(() =>
+    private bool CanRemoveSelected()
+    {
+        return Items.Any(item => item.IsSelected);
+    }
+
+    [RelayCommand]
+    private void SelectionChanged()
+    {
+        RemoveSelectedCommand.NotifyCanExecuteChanged();
+    }
+
+    [RelayCommand(CanExecute = nameof(CanRemoveSelected))]
+    private void RemoveSelected()
+    {
+        var allItems = Items.ToList();
+        var items = Items.Where(item => item.IsSelected).ToList();
+        foreach (var customDataGridStudent in items)
         {
-            var allItems = Items.ToList();
-            var items = Items.Where(item => item.IsSelected).ToList();
-            foreach (var customDataGridStudent in items)
-            {
-                allItems.Remove(customDataGridStudent);
-            }
+            allItems.Remove(customDataGridStudent);
+        }
 
-            Items.Clear();
-            Items.AddRange(allItems);
-        });
+        Items.Clear();
+        Items.AddRange(allItems);
     }
 
     private async Task Initialize()
