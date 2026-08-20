ae8a947188dea221d4e8f628f939ff009625a097
karlgodtliebsen <karl@godtliebsen.dk>
2026-08-20 03:33:57 +0200

    Name updated

--- src/UI/MissionPlanner.App/Views/InitSetup/OptionalHardware/Sections/AirspeedViewModel.cs
+++ src/UI/MissionPlanner.App/Views/InitSetup/OptionalHardware/Sections/AirspeedViewModel.cs
@@ -3,4 +3,9 @@ using MissionPlanner.Core.Vehicles.Abstractions;
 
 namespace MissionPlanner.App.Views.InitSetup.OptionalHardware.Sections;
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="v"></param>
+/// <param name="s"></param>
 public sealed class AirspeedViewModel(IActiveVehicleContext v, IOptionalHardwareService s) : ParameterHardwareViewModel("airspeed", v, s);
---
+++ src/UI/MissionPlanner.App/Views/InitSetup/OptionalHardware/Sections/OpticalFlowView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using UraniumUI.Material.TabViews;
+
+namespace MissionPlanner.App.Views.InitSetup.OptionalHardware.Sections;
+
+public partial class OpticalFlowView : TabViewLifecycleContent<OpticalFlowViewModel>
+{
+    public OpticalFlowView()
+    {
+        InitializeComponent();
+    }
+}
