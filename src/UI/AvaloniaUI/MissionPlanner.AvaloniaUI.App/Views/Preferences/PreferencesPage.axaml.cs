f03fdd1a21d81b9722c125229758f5071121fac0
karlgodtliebsen <karl@godtliebsen.dk>
2026-09-02 06:02:17 +0200

    Migrate Avalonia preferences page

--- src/UI/AvaloniaUI/MissionPlanner.AvaloniaUI.App/Views/Preferences/PreferencesPage.axaml.cs
+++ src/UI/AvaloniaUI/MissionPlanner.AvaloniaUI.App/Views/Preferences/PreferencesPage.axaml.cs
@@ -1,9 +1,11 @@
-﻿using MissionPlanner.AvaloniaUI.App.Utilities;
+using MissionPlanner.AvaloniaUI.App.Utilities;
 
 namespace MissionPlanner.AvaloniaUI.App.Views.Preferences;
 
+/// <summary>Displays application preferences.</summary>
 public partial class PreferencesPage : NavigationViewBase<PreferencesViewModel>
 {
+    /// <summary>Initializes the preferences page.</summary>
     public PreferencesPage()
     {
         InitializeComponent();
